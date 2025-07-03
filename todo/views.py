4846dd38589b94d33a3f7bad8bd734536c24258f
1F10240110 <s1f102401102@iniad.org>
2025-07-03 11:31:23 +0900

    tesut結果より修正

--- todo/views.py
+++ todo/views.py
@@ -13,7 +13,7 @@ def index(request):
         task.save()
 
     if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at')
+        tasks = Task.objects.order_by('-due_at')
     else:
         tasks = Task.objects.order_by('-posted_at')
 
